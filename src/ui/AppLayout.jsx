3a66662effccaa5b601c14bec54b3ede6b94ba3b
Swati Ranjan <swatirp83@gmail.com>
2023-08-02 20:44:59 +0530

    second

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,14 +7,16 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
       {isLoading && <Loader />}
 
       <Header />
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
+
+      <div>
+        <main>
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
