172b4f1cc2c83bb6cbfb1524bdef5d7eb156d903
Swati Ranjan <swatirp83@gmail.com>
2023-08-04 19:15:07 +0530

    fifth commit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+  if (!username) return;
   return (
-    <div className="hidden text-sm font-semibold md:block">Swati Ranjan</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
